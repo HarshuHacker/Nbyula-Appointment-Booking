05c9ef6d65772552cdb40e7eb370ea7012cfe65d
Harsh Prasad <harshisindian@gmail.com>
2022-09-18 16:10:36 +0530

    Route To Get All Users

--- Routes/users.js
+++ Routes/users.js
@@ -5,5 +5,6 @@ const usersController = require('../Controllers/userController')
 
 router.post('/register', usersController.register)
 router.post("/login", usersController.login)
+router.get('/all-user', usersController.allUser)
 
 module.exports = router
