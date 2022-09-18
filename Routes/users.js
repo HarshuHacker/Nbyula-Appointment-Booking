96a095d744a943d9b060678b030da768f2d04dc0
Harsh Prasad <harshisindian@gmail.com>
2022-09-19 00:57:21 +0530

    Route For Update Profile

--- Routes/users.js
+++ Routes/users.js
@@ -5,6 +5,7 @@ const usersController = require('../Controllers/userController')
 
 router.post('/register', usersController.register)
 router.post("/login", usersController.login)
+router.post('/update-profile', usersController.updateProfile)
 router.get('/all-user', usersController.allUser)
 router.get('/one-user', usersController.oneUser)
 
