f04bf7826e4b0fb018a8f2c055d661af6d9b3fda
Harsh Prasad <harshisindian@gmail.com>
2022-09-18 16:05:20 +0530

    Login Action Created

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -3,11 +3,13 @@ const User = require("../Models/users")
 module.exports.register = async function(req, res){
   try {
     
+    // Checking For The Email Id InThe Database
     let user = await User.findOne({email: req.body.email})
 
-    // Check If User Already Has An Account Or Not
+    // If The Email Id Does't Exist In The Database
     if(!user) {
       try {
+        // Creating New User
         await User.create(req.body) 
         return res.json(200, {
           message: "Created An Account Successfully"
@@ -18,6 +20,7 @@ module.exports.register = async function(req, res){
         })
       }
     } else {
+      // A User Already Exist With This Email Id
       return res.json(200, {
         message: "User Already Exists !!"
       })
@@ -25,6 +28,38 @@ module.exports.register = async function(req, res){
   } catch (err) {
     return res.json(200, {
       message: "Error In Finding User In Signing Up"
+    })
+  }
+}
+
+module.exports.login = async function(req,res) {
+  try {
+    // Finding User Using The Email Id
+    let user = await User.findOne({
+      email: req.body.email
+    })
+
+    // If Passwords Does't Match
+    if(!user || user.password != req.body.password){
+      return res.json(422, {
+        message: "Invalid Usertname Or Password"
+      })
+    }
+  
+    // If The User Details Are Correct
+    return res.json(200, {
+      message: "Sign In Successfully",
+      data: {
+        name: user.name,
+        email: user.email
+      }
+    })
+  }
+  catch(err)
+  {
+    console.log("Error : ",err)
+    return res.json(500, {
+      message: "Internal Server Error"
     })
   }
 }
